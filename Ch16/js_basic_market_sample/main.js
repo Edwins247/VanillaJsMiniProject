0136bd7f2b5d18a9650aa5acde23477c75eeabb2
Edwins247 <cheewr85@daum.net>
2025-03-27 20:38:32 +0900

    데이터 연결

--- Ch16/js_basic_market_sample/main.js
+++ Ch16/js_basic_market_sample/main.js
@@ -1,30 +1,14 @@
 import { getProductSection } from "./module/productSection.js";
 
-const productSection = getProductSection('인기 상품', [
-    {
-        "id": 1,
-        "imgSrc": "/js_basic_market/public/assets/파프리카.jpg",
-        "name": "파프리카 2입",
-        "discountPercent": 20,
-        "price": 2000,
-        "originalPrice": 2500
-    },
-    {
-        "id": 2,
-        "imgSrc": "/js_basic_market/public/assets/파스타.jpg",
-        "name": "파스타",
-        "discountPercent": 10,
-        "price": 1800,
-        "originalPrice": 2000
-    },
-    {
-        "id": 3,
-        "imgSrc": "/js_basic_market/public/assets/키위.jpg",
-        "name": "키위",
-        "discountPercent": 10,
-        "price": 2700,
-        "originalPrice": 3000
-    }
-]);
+try {
+  const response = await fetch("public/mock/sectionListData.json");
+  const data = await response.json();
+  const sectionInfoList = data.sectionInfoList;
 
-document.body.appendChild(productSection);
+  sectionInfoList.forEach((sectionInfo) => {
+    const productSectionDOM = getProductSection(sectionInfo);
+    document.body.appendChild(productSection);
+  })
+} catch (error) {
+  console.log(error);
+}
