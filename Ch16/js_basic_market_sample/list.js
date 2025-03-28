4f8b806b6f32d3b86f829e9572dace6db1a6690a
Edwins247 <cheewr85@daum.net>
2025-03-28 12:08:22 +0900

    리스트 페이지 필터 구현

--- Ch16/js_basic_market_sample/list.js
+++ Ch16/js_basic_market_sample/list.js
@@ -1,6 +1,7 @@
 // 물품 목록을 모두 불러와서 페이지에 띄우기 => productList.js getProductList
 
 import { fetchSectionListData } from "./module/fetch.js";
+import { setButtonEvent, setFilterEvent } from "./module/productFilter.js";
 import { getProductList } from "./module/productList.js";
 
 const sectionInfoList = await fetchSectionListData();
@@ -10,3 +11,6 @@ const productList = sectionInfoList.reduce((prev, curr) => [...prev, ...curr.pro
 const section = document.getElementsByTagName('section')[0];
 const productListDOM = getProductList(productList);
 section.appendChild(productListDOM);
+
+setFilterEvent();
+setButtonEvent(productList);
