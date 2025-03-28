3296b86ef29dfe088e547c0c05f8b0d46e592b41
Edwins247 <cheewr85@daum.net>
2025-03-28 14:40:01 +0900

    선택 버튼 구현

--- Ch17/js_basic_event_sample/module/selectCard.js
+++ Ch17/js_basic_event_sample/module/selectCard.js
@@ -1,3 +1,4 @@
+import { SELECT_RESULT_KEY } from "../constants/result.js";
 import { appendChildrenList, makeDOMwithProperties } from "../utils/dom.js";
 
 // JSON -> fetch : 서버에서 내려준다고 가정하거나, 데이터 양이 너무 많아서 코드에 작성하기 부담스러운 경우
@@ -31,16 +32,25 @@ const cardInfoList = [
 ]
 
 const snackCardList = document.getElementsByClassName('snack-card-list')[0];
+const selectButtonDOM = document.getElementsByClassName('participate-button')[0];
+
+const getSelectedCard = () => {
+    return document.getElementsByClassName('select')[0];
+}
+
+const getCardById = (id) => {
+    return document.getElementById(`select-${id}`);
+}
 
 const handleSelectCard = (cardId) => {
     // 선택된 카드를 표시하는 함수
     // 1. 이미 선택되어 있던 카드는 선택 해지
     // 2, 현재 선택한 카드를 선택
 
-    const originalSelectedCard = document.getElementsByClassName('select')[0];
+    const originalSelectedCard = getSelectedCard();
     originalSelectedCard?.classList.remove('select');
 
-    const newSelectedCard = document.getElementById(`select-${cardId}`);
+    const newSelectedCard = getCardById(cardId);
     newSelectedCard?.classList.add('select');
 };
 
@@ -84,4 +94,21 @@ export const setSelectCards = () => {
         const selectCardDOM = getSelectCardDOM(cardInfo);
         snackCardList.appendChild(selectCardDOM);
     })
+}
+
+export const setSelectButton = () => {
+    // 1.버튼 DOM을 받아오기
+    // 2.DOM의 onclick 핸들러 등록
+        // 1) 선택된 카드의 id 찾기
+        // 2) localStorage에 해당 id를 저장
+        // 1번에서 선택된 카드의 id가 없을 때는 선택된 카드가 없다는 경고창을 띄우기
+    selectButtonDOM.onclick = () => {
+        const selectedCard = getSelectedCard();
+        if (!selectedCard) {
+            alert('선택된 카드가 없습니다.');
+            return;
+        }
+        const cardId = selectedCard.id?.split('-')[1];
+        localStorage.setItem(SELECT_RESULT_KEY, cardId);
+    }
 }
